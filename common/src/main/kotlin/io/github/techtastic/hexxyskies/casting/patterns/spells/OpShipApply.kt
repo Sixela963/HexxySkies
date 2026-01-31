a30d3c3fd717de9ea3d2e99452cac4dbada593c7
Sixela963 <alexis@toutut.fr>
2026-01-31 18:13:58 +0100

    New formula for the cost of push and twist spells
    Edited the formula for the cost of push and twist spells
    Adjusted the book entries to reflect that
    (also corrected an error in the description of the twist spells)

--- common/src/main/kotlin/io/github/techtastic/hexxyskies/casting/patterns/spells/OpShipApply.kt
+++ common/src/main/kotlin/io/github/techtastic/hexxyskies/casting/patterns/spells/OpShipApply.kt
@@ -23,6 +23,7 @@ import org.valkyrienskies.mod.common.util.GameToPhysicsAdapter
 import org.valkyrienskies.mod.common.util.toJOML
 import org.valkyrienskies.mod.common.util.toMinecraft
 import kotlin.math.pow
+import kotlin.math.sqrt
 
 class OpShipApply(val type: Type, val reference: Reference): SpellAction {
     override val argc: Int
@@ -45,13 +46,16 @@ class OpShipApply(val type: Type, val reference: Reference): SpellAction {
         if (reference != Reference.BODY)
             pos?.let(env::assertVecInRange)
 
-        val motionForCost = motion.lengthSqr().coerceAtMost(
-            motion.toJOML().dot(
-                when (type) {
-                    Type.FORCE -> target.velocity.min(UNIT_VECTOR, Vector3d())
-                    Type.TORQUE -> target.angularVelocity.min(UNIT_VECTOR, Vector3d())
-                }
-            )).pow(OperatorUtils.checkAndMarkGivenForces(userData, target))
+        //Spell cost function: calibrated to cost 10 dust to accelerate a 1000 kg block at 10 m/s in one cast
+        //The idea is that for a given ship, doubling the speed costs quadruple (like impulse)
+        //but the cost doesn't grow too fast with larger ships
+        val motionForCost = 0.1+ 10*((motion.length() / (10*target.inertiaData.mass*60)).pow(2))* sqrt(target.inertiaData.mass/1000)
+        //Detailed explanation:
+        //0.1 is the base cost
+        //The rest of the formula has 3 parts:
+        //10 is simple the base cost when accelerating a 1000 kg block at 10 m/s in one cast: when doing that,the other two parts should equal one
+        //(motion/(mass*60) is the velocity change from the spell: we divide by 10 to the part is equal to 1 when the change is 10 m/s. The 60 is because the spell is in m/s/kg /s, over a period of 1/60th of a second
+        //sqrt(mass/1000) is the mass multiplier: to allow for larger ships, we only multiply by the sqrt mass. Divided by 1000 to have 1 when applying to a single 1000kg block.
 
         return SpellAction.Result(
             when (type) {
